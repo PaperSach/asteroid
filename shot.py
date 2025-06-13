8f078affb84fcec0236aef9751a47eedf80fe294
Isach <isachelliott@gmail.com>
2025-06-13 17:26:56 -0500

    fixed_bullets

--- shot.py
+++ shot.py
@@ -3,30 +3,20 @@ from constants import *
 from circleshape import CircleShape
 
 class Shot(CircleShape):
-    containers = None
-
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
         self.velocity = pygame.Vector2(0, 0)
-        if self.containers:
-            self.add(*self.containers)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius)
 
     def update(self, dt):
         self.position += self.velocity * dt
-        self.wrap_screen()
 
-    def wrap_screen(self):
-        if self.position.x < 0:
-            self.position.x = SCREEN_WIDTH
-        elif self.position.x > SCREEN_WIDTH:
-            self.position.x = 0
-        if self.position.y < 0:
-            self.position.y = SCREEN_HEIGHT
-        elif self.position.y > SCREEN_HEIGHT:
-            self.position.y = 0
+        # Remove the shot if it goes off the screen
+        if (self.position.x < 0 or self.position.x > SCREEN_WIDTH or
+            self.position.y < 0 or self.position.y > SCREEN_HEIGHT):
+            self.kill()
 
     def collides_with(self, other):
         dist = self.position.distance_to(other.position)
